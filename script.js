060d4e904a531d244d5dec985ac44aa885374876
Alex <s.tsamalashvili1998@gmail.com>
2022-07-22 15:11:50 +0400

    fixed result bug

--- script.js
+++ script.js
@@ -6,26 +6,31 @@ let tip_amount = document.getElementById('tip_amount');
 let eachPersonTotal = document.getElementById('total_bill');
 let customInput = document.getElementById('custom-input');
 let percentButtons = Array.from(document.getElementsByClassName('tip_buttons'));
-
+let percent;
+// document.getElementById('tip_amount').style.color="red";
 function updateInfo(){
-    let customNumber = Number(customInput.value);
     let amount_number = Number(amount.value);
-    if(Number(number_of_people.value)===0){return;} 
-    let tip_for_person = (amount_number*customNumber/100)/Number(number_of_people.value);
-    let total = (amount_number+amount_number*customNumber/100)/Number(number_of_people.value);
+    if(Number(number_of_people.value)===0||!percent){return} 
+    let tip_for_person = (amount_number*percent/100)/Number(number_of_people.value);
+    let total = (amount_number+amount_number*percent/100)/Number(number_of_people.value);
     tip_amount.textContent='$'+tip_for_person.toFixed(2);
     eachPersonTotal.textContent='$'+total.toFixed(2);
+    
+    if(amount.value.length>6){
+        tip_amount.style.fontSize = "30px";
+        eachPersonTotal.style.fontSize = "30px";
+    }
+
+    if(amount.value.length>12){
+        tip_amount.style.fontSize = "20px";
+        eachPersonTotal.style.fontSize = "20px";
+    }
 }
 
 percentButtons.map((button)=>{
     button.addEventListener('click',()=>{
-        let percent = parseInt(button.textContent);
-        let amount_number = Number(amount.value);
-        if(Number(number_of_people.value)===0){return;} 
-        let tip_for_person = (amount_number*percent/100)/Number(number_of_people.value);
-        let total = (amount_number+amount_number*percent/100)/Number(number_of_people.value);
-        tip_amount.textContent='$'+tip_for_person.toFixed(2);
-        eachPersonTotal.textContent='$'+total.toFixed(2);
+        percent = parseInt(button.textContent);
+        updateInfo();
         customInput.value="";
     })
 })
@@ -33,6 +38,7 @@ percentButtons.map((button)=>{
 customInput.addEventListener('input',(event)=>{
     if(customInput.value>0){
         reset_button.style.backgroundColor= "#26C2AE";
+        percent=event.target.value;
         updateInfo();
     }else if(customInput.value<=0){
         customInput.value="";
@@ -49,6 +55,9 @@ amount.addEventListener("input",(event)=>{
 })
 
 number_of_people.addEventListener("input",(event)=>{
+    if(number_of_people.value % 1 != 0){
+        number_of_people.value=Math.round(number_of_people.value);
+    }
     if(event.target.value<=0){
         number_of_people.value=0;
         number_of_people.style.border = "2px solid #E17052";
